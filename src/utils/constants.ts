1e14f87d0b7d0449666388822a8242b4a562749a
22-2 <17890m@protonmail.ch>
2025-09-23 11:01:24 +0900

    style(constants): Update icons for sandbox and in-memory notes

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -13,5 +13,5 @@ export const DEFAULT_SETTINGS: SandboxNotePluginSettings = {
 export const APP_NAME = manifest.name || "SandboxNote";
 export const VIEW_TYPE_SANDBOX = "sandbox-note-view";
 export const VIEW_TYPE_IN_MEMORY = "in-memory-note-view";
-export const SANDBOX_NOTE_ICON = "file-text";
-export const IN_MEMORY_NOTE_ICON = "file-edit";
+export const SANDBOX_NOTE_ICON = "package";
+export const IN_MEMORY_NOTE_ICON = "memory-stick";

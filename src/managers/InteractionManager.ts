ab5f55f00ee1b614332d6b84dfacb2d6aa36a8ad
22-2 <17890m@protonmail.ch>
2025-09-25 17:32:52 +0900

    fix(managers): Temporarily disable obsolete views

--- src/managers/InteractionManager.ts
+++ src/managers/InteractionManager.ts
@@ -18,24 +18,24 @@ export class InteractionManager implements Manager {
 	/** Setup UI elements (ribbon icon and commands) */
 	public load() {
 		// Command to open the sandbox note
-		this.plugin.addCommand({
-			id: "open-sandbox-note-view",
-			name: "Open sandbox note",
-			icon: SANDBOX_NOTE_ICON,
-			callback: () => {
-				this.plugin.activateSandboxView();
-			},
-		});
+		// this.plugin.addCommand({
+		// 	id: "open-sandbox-note-view",
+		// 	name: "Open sandbox note",
+		// 	icon: SANDBOX_NOTE_ICON,
+		// 	callback: () => {
+		// 		this.plugin.activateSandboxView();
+		// 	},
+		// });
 
-		// Command to open the in-memory note
-		this.plugin.addCommand({
-			id: "open-in-memory-note-view",
-			name: "Open in-memory note",
-			icon: IN_MEMORY_NOTE_ICON,
-			callback: () => {
-				this.plugin.activateInMemoryView();
-			},
-		});
+		// // Command to open the in-memory note
+		// this.plugin.addCommand({
+		// 	id: "open-in-memory-note-view",
+		// 	name: "Open in-memory note",
+		// 	icon: IN_MEMORY_NOTE_ICON,
+		// 	callback: () => {
+		// 		this.plugin.activateInMemoryView();
+		// 	},
+		// });
 
 		// Command to open the new hot sandbox note
 		this.plugin.addCommand({
@@ -63,38 +63,38 @@ export class InteractionManager implements Manager {
 			},
 		});
 
-		// Command to save the current note (if it's saveable)
-		this.plugin.addCommand({
-			id: "save-note",
-			name: "Save current note",
-			checkCallback: (checking) => {
-				const view = this.plugin.getActiveAbstractNoteView();
-				if (view) {
-					if (!checking) {
-						view.save();
-					}
-					return true;
-				}
-				return false;
-			},
-		});
+		// // Command to save the current note (if it's saveable)
+		// this.plugin.addCommand({
+		// 	id: "save-note",
+		// 	name: "Save current note",
+		// 	checkCallback: (checking) => {
+		// 		const view = this.plugin.getActiveAbstractNoteView();
+		// 		if (view) {
+		// 			if (!checking) {
+		// 				view.save();
+		// 			}
+		// 			return true;
+		// 		}
+		// 		return false;
+		// 	},
+		// });
 
 		// Ribbon icon to open the sandbox note
-		this.plugin.addRibbonIcon(
-			SANDBOX_NOTE_ICON,
-			"Open sandbox note",
-			() => {
-				this.plugin.activateSandboxView();
-			}
-		);
-		// Ribbon icon to open the in-memory note
-		this.plugin.addRibbonIcon(
-			IN_MEMORY_NOTE_ICON,
-			"Open in-memory note",
-			() => {
-				this.plugin.activateInMemoryView();
-			}
-		);
+		// this.plugin.addRibbonIcon(
+		// 	SANDBOX_NOTE_ICON,
+		// 	"Open sandbox note",
+		// 	() => {
+		// 		this.plugin.activateSandboxView();
+		// 	}
+		// );
+		// // Ribbon icon to open the in-memory note
+		// this.plugin.addRibbonIcon(
+		// 	IN_MEMORY_NOTE_ICON,
+		// 	"Open in-memory note",
+		// 	() => {
+		// 		this.plugin.activateInMemoryView();
+		// 	}
+		// );
 
 		// Ribbon icon to open the hot sandbox note
 		this.plugin.addRibbonIcon(

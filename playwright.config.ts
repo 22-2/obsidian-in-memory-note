167f538f36ba7cadac5b06c580e43b70cca885e2
22-2 <17890m@protonmail.ch>
2025-09-25 23:30:17 +0900

    chore(e2e): Setup end-to-end tests
    Prepare the project for end-to-end testing by adding Playwright dependencies and configuration.
    Includes a basic smoke test spec.

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,22 +1,22 @@
 import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
-    timeout: 120 * 1000,
-    testDir: "./e2e",
-    // コンテナ内の絶対パスに変更
-    outputDir: "/app/test-results",
-    reporter: [["html", { outputFolder: "/app/playwright-report" }]],
-    fullyParallel: false,
-    workers: 1,
-    use: {
-        trace: "on-first-retry",
-        video: "on",
-        screenshot: "only-on-failure",
-    },
-    projects: [
-        {
-            name: "chromium",
-            use: { ...devices["Desktop Chrome"] },
-        },
-    ],
+	timeout: 120 * 1000,
+	testDir: "./src/e2e",
+	// コンテナ内の絶対パスに変更
+	outputDir: "../test-results",
+	reporter: [["html", { outputFolder: "../playwright-report" }]],
+	fullyParallel: false,
+	workers: 1,
+	use: {
+		trace: "on-first-retry",
+		video: "on",
+		screenshot: "only-on-failure",
+	},
+	projects: [
+		{
+			name: "chromium",
+			use: { ...devices["Desktop Chrome"] },
+		},
+	],
 });

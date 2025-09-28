b86ab167175ad6d0476cbb52e924de9ebf438478
22-2 <17890m@protonmail.ch>
2025-09-28 17:48:51 +0900

    remove dead code

--- src/e2e/specs/setup/example.spec.ts
+++ src/e2e/specs/setup/example.spec.ts
@@ -7,11 +7,7 @@ import { VIEW_TYPE_HOT_SANDBOX } from "../../../utils/constants";
 
 import { DIST_DIR, PLUGIN_ID, SANDBOX_VAULT_NAME } from "../../config";
 import { expect, test } from "../../test-fixtures";
-import log from "loglevel";
 
-const logger = log.getLogger("example.spec");
-
-// Sandboxを使用（デフォルト）
 test("sandbox test", async ({ vault }) => {
 	const vaultName = await vault.window.evaluate(() => app.vault.getName());
 	expect(vaultName).toBe(SANDBOX_VAULT_NAME);
